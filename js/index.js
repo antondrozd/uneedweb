e1747b94af81d99f301fcd2e2d545a5a1c825b1a
Kirill Buga <tztimezero@yandex.ru>
2017-09-11 15:03:53 +0300

    WIP

--- js/index.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var navbarButton = document.getElementById('toggle-navbar');
-	var body = document.getElementsByTagName('body')[0];
-	navbarButton.addEventListener('click', function() {
-		body.classList.toggle('navbar-expanded');
-	});
-})(window);
