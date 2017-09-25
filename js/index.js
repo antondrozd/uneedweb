81b929e394eb4ed015a93a05e0e680ad9024af52
Kirill Buga <tztimezero@yandex.ru>
2017-09-25 19:43:27 +0300

    WIP

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@
 
 	var slides = $(".portfolio-slides"),
 		switcherOptions = $('#switcher > a'),
+		sections = $('section'),
 		scrollDown = $('#scroll-down'),
 		blockLinks = $('[data-href]'),
 		swipeshowConfig = {
@@ -34,60 +35,17 @@
 		window.location.href = href;
 	});
 
-	var TxtType = function(el, toRotate, period) {
-		this.toRotate = toRotate;
-		this.el = el;
-		this.loopNum = 0;
-		this.period = parseInt(period, 10) || 2000;
-		this.txt = '';
-		this.tick();
-		this.isDeleting = false;
-	};
+	sections.appear();
 
-	TxtType.prototype.tick = function() {
-		var i = this.loopNum % this.toRotate.length;
-		var fullTxt = this.toRotate[i];
+	sections.on('appear', function(event, elements) {
+		elements.each(function(index, el) {
+			$(el).addClass('visible');
+		});
+	});
 
-		if (this.isDeleting) {
-			this.txt = fullTxt.substring(0, this.txt.length - 1);
-		} else {
-			this.txt = fullTxt.substring(0, this.txt.length + 1);
-		}
-
-		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-		var self = this;
-		var delta = 200 - Math.random() * 100;
-
-		if (this.isDeleting) { delta /= 2; }
-
-		if (!this.isDeleting && this.txt === fullTxt) {
-			delta = this.period;
-			this.isDeleting = true;
-		} else if (this.isDeleting && this.txt === '') {
-			this.isDeleting = false;
-			this.loopNum++;
-			delta = 500;
-		}
-
-		setTimeout(function() {
-			self.tick();
-		}, delta);
-	};
-
-	window.onload = function() {
-		var elements = document.getElementsByClassName('typewrite');
-		for (var i=0; i<elements.length; i++) {
-			var toRotate = elements[i].getAttribute('data-type');
-			var period = elements[i].getAttribute('data-period');
-			if (toRotate) {
-			  new TxtType(elements[i], JSON.parse(toRotate), period);
-			}
-		}
-		// INJECT CSS
-		var css = document.createElement("style");
-		css.type = "text/css";
-		css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-		document.body.appendChild(css);
-	};
+	// sections.on('disappear', function(event, elements) {
+	// 	elements.each(function(index, el) {
+	// 		$(el).removeClass('visible');
+	// 	});
+	// });
 })();
