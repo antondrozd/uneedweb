ea75bc3fc9390db80739b85b992826cada725957
Kirill Buga <tztimezero@yandex.ru>
2017-09-27 19:43:46 +0300

    Animations

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,13 @@
 (function() {
-	var gallery = $(".gallery-container"),
-		swipeshowConfig = {
-			autostart: false,
-			$next: $(".slide-right"),
-			$previous: $(".slide-left")
-		};
-	gallery.swipeshow(swipeshowConfig);
+	window.uneed = window.uneed || {};
+	var filterOptions = ['all', 'web', 'mobile', 'design'],
+		projects = $('#projects'),
+		filterEls = $('#all-filter, #web-filter, #mobile-filter, #design-filter');
+
+	window.uneed.onChangePortfolioFilter = function(filter) {
+		filterEls.removeClass('active');
+		$('#' + filter + '-filter').addClass('active');
+		projects.removeClass();
+		projects.addClass(filter + '-filter');
+	};
 })();
