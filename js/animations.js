f4c199cd42032ef571e0a1f0a4ce05dbea587fdf
Anton Drozd <anton.drozd@uneedweb.com>
2017-12-25 17:31:34 +0200

    add animations-group script

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 
-	var elementToAnimate = $('[data-animation]').css('opacity', 0);
+	var elementToAnimate = $('[data-animation]')
 	elementToAnimate.each(function () {
-		var animationType = $(this).attr('data-animation');
-		var element = $(this);
-		element.waypoint(function() {
-			element.addClass('animated').addClass(animationType).css('opacity', '');
-			console.log(animationType);
-		}, { offset: '70%' });
+		if (!$(this).hasClass('animated')) {
+			$(this).css('opacity', 0);
+			var animationType = $(this).attr('data-animation');
+			var element = $(this);
+			element.waypoint(function () {
+				element
+					.addClass('animated')
+					.addClass(animationType)
+					.css('opacity', '');
+			}, { offset: '70%' });
+		}
+	});
+
+	var animationsGroup = $('[data-animations-group]');
+	animationsGroup.each(function () {
+		$(this).find(elementToAnimate).each(function () {
+			var animationType = $(this).attr('data-animation');
+			var element = $(this);
+			element.waypoint(function () {
+				animationsGroup
+					.find($('[data-animation]'))
+					.addClass('animated')
+					.addClass(animationType)
+					.css('opacity', '');
+			}, { offset: '70%'});
+		});
 	});
 
 });
