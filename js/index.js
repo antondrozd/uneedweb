c1642a2c116eca8218033d8c9ede910a4ffce3cd
Anton Drozd <anton.drozd@uneedweb.com>
2017-12-26 01:59:31 +0200

    add contact-us/success page

--- js/index.js
+++ js/index.js
@@ -7,43 +7,43 @@
 		$(switcherOptions[number]).addClass('active');
 	};
 
-	var slides = $(".portfolio-slides"),
-		switcherOptions = $('#switcher > a'),
-		sections = $('section'),
-		scrollDown = $('#scroll-down'),
-		blockLinks = $('[data-href]'),
-		swipeshowConfig = {
-			autostart: false,
-			$next: $("#switch-right"),
-			$previous: $("#switch-left"),
-			onactivate: function(slide, index) {
-				switcherOptions.removeClass('active');
-				$(switcherOptions[index]).addClass('active');
-			}
-		};
+	// var slides = $(".portfolio-slides"),
+	// 	switcherOptions = $('#switcher > a'),
+	// 	sections = $('section'),
+	// 	scrollDown = $('#scroll-down'),
+	// 	blockLinks = $('[data-href]'),
+	// 	swipeshowConfig = {
+	// 		autostart: false,
+	// 		$next: $("#switch-right"),
+	// 		$previous: $("#switch-left"),
+	// 		onactivate: function(slide, index) {
+	// 			switcherOptions.removeClass('active');
+	// 			$(switcherOptions[index]).addClass('active');
+	// 		}
+	// 	};
 
-	slides.swipeshow(swipeshowConfig);
+	// slides.swipeshow(swipeshowConfig);
 
-	scrollDown.on('click', function() {
-		$('html, body').animate({
-			scrollTop: window.innerHeight
-		}, 800);
-	});
+	// scrollDown.on('click', function() {
+	// 	$('html, body').animate({
+	// 		scrollTop: window.innerHeight
+	// 	}, 800);
+	// });
 
-	blockLinks.on('click', function() {
-		var href = $(this).data('href');
-		window.location.href = href;
-	});
+	// blockLinks.on('click', function() {
+	// 	var href = $(this).data('href');
+	// 	window.location.href = href;
+	// });
 
-	sections.appear();
+	// sections.appear();
 
-	sections.on('appear', function(event, elements) {
-		elements.each(function(index, el) {
-			$(el).addClass('visible');
-		});
-	});
+	// sections.on('appear', function(event, elements) {
+	// 	elements.each(function(index, el) {
+	// 		$(el).addClass('visible');
+	// 	});
+	// });
 
-	$.force_appear();
+	// $.force_appear();
 
 	$('a[href^="#"]:not([href="#"])').click(function (event) {
 		event.preventDefault();
