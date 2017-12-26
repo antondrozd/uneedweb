ad6a5ccbb8824cb19d5da12b61e32586bfb9ceb2
Anton <anton@minis-Mac-mini.local>
2017-12-26 18:56:47 +0200

    add project filter script

--- js/animations.js
+++ js/animations.js
@@ -15,19 +15,19 @@ $(document).ready(function () {
 		};
 	});
 
-	// var animationsGroup = $('[data-animations-group]');
-	// animationsGroup.each(function () {
-	// 	var group = $(this);
-	// 	group.find(elementToAnimate).each(function () {
-	// 		var element = $(this);
-	// 		var animationType = element.attr('data-animation');
-	// 		group.waypoint(function () {
-	// 			element
-	// 				.addClass('animated')
-	// 				.addClass(animationType)
-	// 				.css('opacity', '');
-	// 		}, {offset: '90%'});
-	// 	});
-	// });
+	var animationsGroup = $('[data-animations-group]');
+	animationsGroup.each(function () {
+		var group = $(this);
+		group.find(elementToAnimate).each(function () {
+			var element = $(this);
+			var animationType = element.attr('data-animation');
+			group.waypoint(function () {
+				element
+					.addClass('animated')
+					.addClass(animationType)
+					.css('opacity', '');
+			}, {offset: '100%'});
+		});
+	});
 
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,49 +1,74 @@
-// (function() {
-// 	window.uneed = window.uneed || {};
-// 	var filterOptions = ['all', 'web', 'mobile', 'design'],
-// 		projects = $('#projects'),
-// 		slides = $(".gallery-container"),
-// 		activeFilter = 'all',
-// 		filterEls = $('#all-filter, #web-filter, #mobile-filter, #design-filter'),
-// 		swipeshowConfig = {
-// 			autostart: false,
-// 			$next: $("#switch-right"),
-// 			$previous: $("#switch-left")
-// 		};
+// (function filter () {
 
-// 	slides.swipeshow(swipeshowConfig);
+	var filterOptions = ['all', 'web', 'mobile', 'design'],
+			gallery = document.querySelector('.projects-gallery'),
+			galleryItems = Array.prototype.slice.call(gallery.querySelectorAll('.project-item')),
+			gallerySwitcher = gallery.querySelector('.switcher'),
+			filterItems = gallerySwitcher.querySelectorAll('.switcher__name'),
+			filterSwitchPrev = gallerySwitcher.querySelector('.switcher__btn--prev'),
+			filterSwitchNext = gallerySwitcher.querySelector('.switcher__btn--next'),
+			currentFilter = 'all',
+			currentFilterIndex = 0,
+			firstRendering = true;
 
-// 	window.uneed.onChangePortfolioFilter = function(filter) {
-// 		filterEls.removeClass('active');
-// 		$('#' + filter + '-filter').addClass('active');
-// 		projects.removeClass();
-// 		projects.addClass(filter + '-filter');
-// 		activeFilter = filter;
-// 	};
-// 	window.uneed.onPrevPortfolioFilter = function() {
-// 		var currentIndex = filterOptions.indexOf(activeFilter);
-// 		if (currentIndex === 0) {
-// 			currentIndex = filterOptions.length - 1;
-// 		}
-// 		var prevIndex = currentIndex - 1;
-// 		window.uneed.onChangePortfolioFilter(filterOptions[prevIndex]);
-// 	};
-// 	window.uneed.onNextPortfolioFilter = function() {
-// 		var currentIndex = filterOptions.indexOf(activeFilter);
-// 		if (currentIndex === filterOptions.length - 1) {
-// 			currentIndex = 0;
-// 		}
-// 		var nextIndex = currentIndex + 1;
-// 		window.uneed.onChangePortfolioFilter(filterOptions[nextIndex]);
-// 	};
-// })();
+	filterItems.forEach(function (item) {
+		item.addEventListener('click', function (event) {
+			var type = event.target.getAttribute('data-type');
+			onFilterChange(type);
+			item.classList.add('switcher__name--active');
+		});
+	});
 
+	function onFilterSwitch(index) {
+		var newFilter = filterOptions[index];
+		var filterEl = document.querySelector('[data-type="' + newFilter + '"]');
+		filterEl.classList.add('switcher__name--active');
+		onFilterChange(newFilter);
+	}
 
+	filterSwitchPrev.addEventListener('click', function () {
+		var newIndex = currentFilterIndex - 1;
+		if (newIndex < 0) {
+			newIndex = filterOptions.length - 1;
+		}
+		onFilterSwitch(newIndex);
+	});
 
-// (function () {
+	filterSwitchNext.addEventListener('click', function () {
+		var newIndex = currentFilterIndex + 1;
+		if (newIndex > filterOptions.length - 1) {
+			newIndex = 0;
+		}
+		onFilterSwitch(newIndex);
+	});
 
-var filterOptions = ['all', 'web', 'mobile', 'design'],
-		gallery = document.querySelector('.projects-gallery__list'),
-		galleryItems = gallery.querySelectorAll('.project-item');
+	function onFilterChange (filter) {
+		var result = galleryItems;
+		if (filter !== 'all') {
+			result = galleryItems.filter(function (item) {
+				var types = item.getAttribute('data-type').split(' ');
+				return types.indexOf(filter) > -1;
+			});
+		}
+		galleryItems.forEach(function (item) {
+			item.classList.remove('animated');
+			item.classList.remove('project-item--visible');
+			item.classList.add('animated');
+		});
+		result.forEach(function (item) {
+			item.classList.add('project-item--visible');
+		});
+		// filterItems.forEach(function (item) {
+		// 	item.classList.remove('switcher__name--active');
+		// });
+		if (!firstRendering) {
+			filterItems[currentFilterIndex].classList.remove('switcher__name--active');
+			currentFilter = filter;
+			currentFilterIndex = filterOptions.indexOf(currentFilter);
+		}
+	};
+
+	onFilterChange('all');
+	firstRendering = false;
 
 // })();
