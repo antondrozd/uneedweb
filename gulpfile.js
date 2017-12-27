66f899a014ee3126e226104ebe725b34fbb81c1f
Anton <anton@minis-Mac-mini.local>
2017-12-27 19:20:35 +0200

    on-game table fix, new images

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var imagemin = require('gulp-tinypng');
 
 gulp.task('tinypng', function(cb) {
-	gulp.src('images/**/twitter.{png,jpg}')
+	gulp.src('**/on-game/bg-1.{png,jpg}')
 		.pipe(imagemin('ZiZYm9giK0yP_007ofWJgQT5YaUXDy2Y'))
 		.pipe(gulp.dest('tiny'));
 });
