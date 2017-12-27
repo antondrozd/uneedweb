a1bf9a90ec7588fbf3c08434bec01c8c14e10a29
Anton <anton@minis-Mac-mini.local>
2017-12-27 17:41:10 +0200

    twitter icon optimized

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var imagemin = require('gulp-tinypng');
 
 gulp.task('tinypng', function(cb) {
-	gulp.src('images/**/*.{png,jpg}')
+	gulp.src('images/**/twitter.{png,jpg}')
 		.pipe(imagemin('ZiZYm9giK0yP_007ofWJgQT5YaUXDy2Y'))
 		.pipe(gulp.dest('tiny'));
 });
