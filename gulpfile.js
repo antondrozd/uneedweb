a4d30af47ebaa82e7d54a7bb651e5a94130e518f
Anton Drozd <antondrozd@Pros-MBP.connectify>
2017-12-28 03:06:13 +0200

    prefix "-b" deleted

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-tinypng');
 
-gulp.task('tinypng', function(cb) {
+gulp.task('tinypng', function (cb) {
+
 	gulp.src('**/on-game/bg-1.{png,jpg}')
 		.pipe(imagemin('ZiZYm9giK0yP_007ofWJgQT5YaUXDy2Y'))
 		.pipe(gulp.dest('tiny'));
+
 });
