9ced5334912db21877c6fb2e911adec1818665d0
Anton Drozd <antondrozd@Pros-MacBook-Pro.local>
2017-12-29 01:41:48 +0200

    update slider

--- js/swipeshow.js
+++ js/swipeshow.js
@@ -1,15 +1,24 @@
 $(function () {
 
-	var sliderObjects = {
-		slider: $('.slider'),
-		controls: $('.switcher'),
-		previous: $('.switcher__btn--prev'),
-		next: $('.switcher__btn--next'),
-		slides: $('.slider__slides-wrapper'),
-		slide: $('.slider__slide'),
-		dots: $('.slider__dots')
-	},
+	var switcherOptions = $('.switcher__name'),
+			sliderObjects = {
+				slider: $('.slider'),
+				controls: $('.switcher'),
+				previous: $('.switcher__btn--prev'),
+				next: $('.switcher__btn--next'),
+				slides: $('.slider__slides-wrapper'),
+				slide: $('.slider__slide'),
+				dots: $('.slider__dots')
+			},
 			slidesCount = $(sliderObjects.slide).length;
+
+	$(switcherOptions).on('click', function () {
+		var index = $(this).index() - 1;
+		$(sliderObjects.slider).swipeshow().goTo(index);
+		$(switcherOptions).removeClass('switcher__name--active');
+		$(switcherOptions[index]).addClass('switcher__name--active');
+	});
+
 
 	$(sliderObjects.slides).css('width', slidesCount*100+'%');
 
@@ -19,9 +28,13 @@ $(function () {
 
 
 	$(sliderObjects.slider).swipeshow({
-		autostart: false
+		autostart: false,
+		onactivate: function(slide, index) {
+			$(switcherOptions).removeClass('switcher__name--active');
+			$(switcherOptions[index]).addClass('switcher__name--active');
+		}
 	});
 
 	$('.dot-item').addClass('slider__dot');
-	
+
 });
